c6787d81193afb7eaf000271f75ae1252191033b
edodima13 <edodima13@libero.it>
2022-02-22 16:45:38 +0100

    avanzamento

--- myjavascript.js
+++ myjavascript.js
@@ -1,26 +1,35 @@
-const title = [
-    'Svezia',
-    'Svizzera',
-    'Gran Bretagna',
-    'Germania',
-    'Paradise'
-]
-
-const text = [
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
-    'Lorem ipsum',
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-    'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-]
-
-const items = [
-    'img/01.jpg',
-    'img/02.jpg',
-    'img/03.jpg',
-    'img/04.jpg',
-    'img/05.jpg'
+const card = [
+    {
+        title : 'Svezia',
+        text : 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+        items :'img/01.jpg',
+    },
+    {
+        title : 'Svizzera',
+        text : 'Lorem ipsum',
+        items :'img/02.jpg',
+    },
+    {
+        title : 'Gran Bretagna',
+        text : 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+        items :'img/03.jpg',
+    },
+    {
+        title : 'Germania',
+        text : 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+        items :'img/04.jpg',
+    },
+    {
+        title : 'Paradise',
+        text : 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+        items :'img/05.jpg',
+    }
 ];
+
+
+    const title = [];
+    const text = [];
+    const items = [];
 
     const containerLeft = document.getElementsByClassName("container")[0];
     const containerRight = document.getElementsByClassName("container")[1];
